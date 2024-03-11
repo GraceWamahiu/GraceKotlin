cbc06df4b8e4a1994a8b995b7cfe837a2770a8c8
GraceWamahiu <wamahiugrace00@gmail.com>
2024-03-11 13:12:28 +0300

    init

---
+++ src/introduction.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Grace")
+    println(20)
+    println(20+2)
+    println("eMobilis")
+    print("Grace")
+    print("Wamahiu")
+}
---
+++ src/variables.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var firstname = "Susan"
+    firstname = "Grace"
+
+    val pi = 3.14
+    var lastname = "Chomba"
+    var school = "eMobilis"
+
+    println(firstname+ " " + lastname)
+    println("My First name is " +firstname+ ".I study at " +school )
+
+}
